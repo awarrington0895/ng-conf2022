6397b3899ca38346796feadc786282a8961a691e
Alex <awarrington0895@gmail.com>
2022-09-07 02:38:33 +0000

    Add tour of heroes app

---
+++ apps/tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'ngconf-root',
+  template: ``,
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tour-of-heroes';
+}
