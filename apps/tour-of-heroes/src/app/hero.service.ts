b3713250d4435c9033b1823fdd8de04ac844a2b6
Alex <awarrington0895@gmail.com>
2022-09-09 02:36:38 +0000

    Add in-memory service to use real httpclient

--- apps/tour-of-heroes/src/app/hero.service.ts
+++ apps/tour-of-heroes/src/app/hero.service.ts
@@ -1,29 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
-import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
+
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-
-    this.messageService.add('HeroService: fetched heroes');
-
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(() => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const hero = HEROES.find((h) => h.id === id)!;
+    const url = `${this.heroesUrl}/${id}`;
 
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-
-    return of(hero);
+    return this.http.get<Hero>(url).pipe(
+      tap(() => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
-  constructor(private messageService: MessageService) {}
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
 }
--- apps/tour-of-heroes/src/main.ts
+++ apps/tour-of-heroes/src/main.ts
@@ -1,7 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from './app/app.component';
+import { InMemoryDataService } from './app/in-memory-data.service';
 import { routes } from './app/routes';
 import { environment } from './environments/environment';
 
@@ -10,5 +13,13 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(RouterModule.forRoot(routes))],
+  providers: [
+    importProvidersFrom(
+      HttpClientModule,
+      RouterModule.forRoot(routes),
+      HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+        dataEncapsulation: false,
+      })
+    ),
+  ],
 }).catch((err) => console.error(err));
