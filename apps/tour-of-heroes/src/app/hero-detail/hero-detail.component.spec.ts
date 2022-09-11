e40a5908c85081c3988ce6b5cecac0102026f3db
Alex <awarrington0895@gmail.com>
2022-09-11 18:41:38 +0000

    Add missing tests

---
+++ apps/tour-of-heroes/src/app/hero-detail/hero-detail.component.spec.ts
@@ -0,0 +1,37 @@
+import {
+  ActivatedRoute,
+  ActivatedRouteSnapshot,
+  convertToParamMap,
+} from '@angular/router';
+import { MockBuilder, MockRender } from 'ng-mocks';
+import { of } from 'rxjs';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { HeroDetailComponent } from './hero-detail.component';
+
+const mockActivatedRoute = {
+  snapshot: {
+    paramMap: convertToParamMap({ id: 1 }),
+  } as ActivatedRouteSnapshot,
+};
+
+const mockHero: Hero = {
+  id: 1,
+  name: 'Mock',
+};
+
+const mockHeroService = {
+  getHero: () => of(mockHero),
+};
+
+describe('HeroDetailComponent', () => {
+  beforeEach(() =>
+    MockBuilder(HeroDetailComponent)
+      .mock(ActivatedRoute, mockActivatedRoute)
+      .mock(HeroService, mockHeroService)
+  );
+
+  it('should create', () => {
+    expect(MockRender(HeroDetailComponent)).toBeTruthy();
+  });
+});
