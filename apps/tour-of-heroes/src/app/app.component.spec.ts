56c87854dfc9d3890a8d6d37b39722cc3ebd0acd
Alex <awarrington0895@gmail.com>
2022-09-11 18:46:36 +0000

    Fix app component test

--- apps/tour-of-heroes/src/app/app.component.spec.ts
+++ apps/tour-of-heroes/src/app/app.component.spec.ts
@@ -1,8 +1,15 @@
+import { provideMockStore } from '@ngrx/store/testing';
 import { MockBuilder, MockRender } from 'ng-mocks';
 import { AppComponent } from './app.component';
 
+const initialState = {
+  heroes: [],
+};
+
 describe('AppComponent', () => {
-  beforeEach(() => MockBuilder(AppComponent));
+  beforeEach(() =>
+    MockBuilder(AppComponent).provide(provideMockStore({ initialState }))
+  );
 
   it('should create the app', () => {
     expect(MockRender(AppComponent)).toBeTruthy();
