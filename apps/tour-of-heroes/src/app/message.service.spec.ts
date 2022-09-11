3adb7b99e84b08deeee72f55e23143ed1a49259e
Alex <awarrington0895@gmail.com>
2022-09-11 21:52:42 +0000

    Fix failing tests

--- apps/tour-of-heroes/src/app/message.service.spec.ts
+++ apps/tour-of-heroes/src/app/message.service.spec.ts
@@ -1,27 +1,50 @@
+import { TestBed } from '@angular/core/testing';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
+import { StoreModule } from '@ngrx/store';
+import { heroesReducer } from './+state/heroes/hero.reducer';
+import { messagesReducer } from './+state/messages/messages.reducer';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
-  let service: MessageService;
-
-  beforeEach(() => {
-    service = new MessageService();
-  });
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [
+        StoreModule.forRoot({
+          heroes: heroesReducer,
+          messages: messagesReducer,
+        }),
+      ],
+      providers: [MessageService],
+    }).compileComponents()
+  );
 
   it('should have an empty set of messages to read initially', () => {
-    expect(service.messages).toStrictEqual([]);
+    const service = TestBed.inject(MessageService);
+
+    const messageSpy = subscribeSpyTo(service.messages$);
+
+    expect(messageSpy.getValues()).toStrictEqual([[]]);
   });
 
   it('should have a way to add messages', () => {
+    const service = TestBed.inject(MessageService);
+
     service.add('test');
 
-    expect(service.messages).toStrictEqual(['test']);
+    const messagesSpy = subscribeSpyTo(service.messages$);
+
+    expect(messagesSpy.getLastValue()).toStrictEqual(['test']);
   });
 
   it('should have a way to clear the messages', () => {
+    const service = TestBed.inject(MessageService);
+
     service.add('test');
 
     service.clear();
 
-    expect(service.messages).toStrictEqual([]);
+    const messagesSpy = subscribeSpyTo(service.messages$);
+
+    expect(messagesSpy.getLastValue()).toStrictEqual([]);
   });
 });
--- apps/tour-of-heroes/src/app/messages/messages.component.spec.ts
+++ apps/tour-of-heroes/src/app/messages/messages.component.spec.ts
@@ -1,11 +1,12 @@
 import { MockBuilder, MockRender } from 'ng-mocks';
+import { of } from 'rxjs';
 import { MessageService } from '../message.service';
 
 import { MessagesComponent } from './messages.component';
 
 describe('MessagesComponent', () => {
   beforeEach(() =>
-    MockBuilder(MessagesComponent).mock(MessageService, { messages: [] })
+    MockBuilder(MessagesComponent).mock(MessageService, { messages$: of([]) })
   );
 
   it('should create', () => {
